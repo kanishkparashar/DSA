754a4884d89762a8ab16bfe1f7a611c616490d7e
Kanishk parashar <143627169+Pt-kanishk@users.noreply.github.com>
2026-04-02 21:43:03 +0530

    Time: 100 ms (68.54%) | Memory: 238.1 MB (77.53%) - LeetSync

---
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maximumAmount(int[][] coins) {
+        int m=coins.length,n=coins[0].length;
+        int[][][] dp=new int[m][n][3];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<3;k++){
+                    dp[i][j][k]=Integer.MIN_VALUE;
+                }
+            }
+        }
+        if(coins[0][0]>=0){
+            dp[0][0][0]=coins[0][0];
+        }else{
+            dp[0][0][0]=coins[0][0];
+            dp[0][0][1]=0;
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<3;k++){
+                    if(i==0&&j==0)continue;
+                    int val=coins[i][j];
+                    int bestPrev=Integer.MIN_VALUE;
+                    if(i>0)bestPrev=Math.max(bestPrev,dp[i-1][j][k]);
+                    if(j>0)bestPrev=Math.max(bestPrev,dp[i][j-1][k]);
+                    if(bestPrev!=Integer.MIN_VALUE){
+                        dp[i][j][k]=Math.max(dp[i][j][k],bestPrev+val);
+                    }
+                    if(val<0&&k>0){
+                        int bestPrevNeutral=Integer.MIN_VALUE;
+                        if(i>0)bestPrevNeutral=Math.max(bestPrevNeutral,dp[i-1][j][k-1]);
+                        if(j>0)bestPrevNeutral=Math.max(bestPrevNeutral,dp[i][j-1][k-1]);
+                        if(bestPrevNeutral!=Integer.MIN_VALUE){
+                            dp[i][j][k]=Math.max(dp[i][j][k],bestPrevNeutral);
+                        }
+                    }
+                }
+            }
+        }
+        return Math.max(dp[m-1][n-1][0],Math.max(dp[m-1][n-1][1],dp[m-1][n-1][2]));
+    }
+}
