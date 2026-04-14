f1035d5ceafbe7ce7f68756b7e9621d7da01059f
Kanishk parashar <143627169+Pt-kanishk@users.noreply.github.com>
2026-04-14 22:09:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -0,0 +1,9 @@
+class Solution{
+	String removeSpaces(String s){
+		StringBuilder sb=new StringBuilder();
+		for(int i=0;i<s.length();i++){
+			if(s.charAt(i)!=' ')sb.append(s.charAt(i));
+		}
+		return sb.toString();
+	}
+}
