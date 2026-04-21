dafba30ded6e3e4e92a8dd0db13f6ba18e6ff30a
Kanishk parashar <143627169+Pt-kanishk@users.noreply.github.com>
2026-04-21 21:36:20 +0530

    Time: 47 ms (69.89%) | Memory: 104.3 MB (97.85%) - LeetSync

--- 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.java
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.java
@@ -1,50 +1,38 @@
-class Solution {
-    class DSU {
-        int[] p;
-
-        DSU(int n) {
-            p = new int[n];
-            for (int i = 0; i < n; i++) p[i] = i;
-        }
-
-        int f(int x) {
-            if (p[x] != x) p[x] = f(p[x]);
-            return p[x];
-        }
-
-        void u(int a, int b) {
-            int pa = f(a), pb = f(b);
-            if (pa != pb) p[pa] = pb;
-        }
-    }
-
-    public int minimumHammingDistance(int[] s, int[] t, int[][] a) {
-        int n = s.length;
-        DSU d = new DSU(n);
-
-        for (int[] x : a) d.u(x[0], x[1]);
-
-        java.util.Map<Integer, java.util.List<Integer>> m = new java.util.HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            int p = d.f(i);
-            m.computeIfAbsent(p, k -> new java.util.ArrayList<>()).add(i);
-        }
-
-        int r = 0;
-
-        for (java.util.List<Integer> g : m.values()) {
-            java.util.Map<Integer, Integer> f = new java.util.HashMap<>();
-
-            for (int i : g) f.put(s[i], f.getOrDefault(s[i], 0) + 1);
-
-            for (int i : g) {
-                int v = t[i];
-                if (f.getOrDefault(v, 0) > 0) f.put(v, f.get(v) - 1);
-                else r++;
-            }
-        }
-
-        return r;
-    }
+class Solution{
+	class DSU{
+		int[]p;
+		DSU(int n){
+			p=new int[n];
+			for(int i=0;i<n;i++)p[i]=i;
+		}
+		int f(int x){
+			if(p[x]!=x)p[x]=f(p[x]);
+			return p[x];
+		}
+		void u(int a,int b){
+			int pa=f(a),pb=f(b);
+			if(pa!=pb)p[pa]=pb;
+		}
+	}
+	public int minimumHammingDistance(int[]s,int[]t,int[][]a){
+		int n=s.length;
+		DSU d=new DSU(n);
+		for(int[]x:a)d.u(x[0],x[1]);
+		java.util.Map<Integer,java.util.List<Integer>>m=new java.util.HashMap<>();
+		for(int i=0;i<n;i++){
+			int p=d.f(i);
+			m.computeIfAbsent(p,k->new java.util.ArrayList<>()).add(i);
+		}
+		int r=0;
+		for(java.util.List<Integer>g:m.values()){
+			java.util.Map<Integer,Integer>f=new java.util.HashMap<>();
+			for(int i:g)f.put(s[i],f.getOrDefault(s[i],0)+1);
+			for(int i:g){
+				int v=t[i];
+				if(f.getOrDefault(v,0)>0)f.put(v,f.get(v)-1);
+				else r++;
+			}
+		}
+		return r;
+	}
 }
