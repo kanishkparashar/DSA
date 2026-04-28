294013a500c584764f01c33e5f49c28b9d433b25
Kanishk parashar <143627169+Pt-kanishk@users.noreply.github.com>
2026-04-28 17:02:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Repeating Character Replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,21 @@
+class Solution {
+	public int longestSubstr(String s,int k) {
+		int[] freq=new int[26];
+		int left=0,maxCount=0,ans=0;
+
+		for(int right=0;right<s.length();right++) {
+			int idx=s.charAt(right)-'A';
+			freq[idx]++;
+			maxCount=Math.max(maxCount,freq[idx]);
+
+			while((right-left+1)-maxCount>k) {
+				freq[s.charAt(left)-'A']--;
+				left++;
+			}
+
+			ans=Math.max(ans,right-left+1);
+		}
+
+		return ans;
+	}
+}
