1337c417071bcceb446cf7107c0115794884c622
Kanishk parashar <143627169+kanishkparashar@users.noreply.github.com>
2026-05-17 18:44:05 +0530

    Time: 1 ms (99.73%) | Memory: 52.9 MB (28.33%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,12 @@
+class Solution{
+	public boolean canReach(int[]arr,int start){
+		boolean[]vis=new boolean[arr.length];
+		return dfs(arr,start,vis);
+	}
+	public boolean dfs(int[]arr,int i,boolean[]vis){
+		if(i<0||i>=arr.length||vis[i])return false;
+		if(arr[i]==0)return true;
+		vis[i]=true;
+		return dfs(arr,i+arr[i],vis)||dfs(arr,i-arr[i],vis);
+	}
+}
